b2bc84e11e96c927f1ce0a3c5b38d43ffd26c5a7
Marwan Hasbun Nur <hasbunurmarwan10@gmail.com>
2024-03-08 21:58:40 +0700

    chore: login form

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -5,10 +5,31 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C1A29),
       body: Center(
-        child: Text('Welcome to Login Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+                'assets/images/logo.png'), // Tidak bisa menggunakan 'const' di sini
+            Text(
+              'Masuk',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            TextFormField(),
+            TextFormField(),
+          ],
+        ),
       ),
+      floatingActionButton: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 12),
+          width: double.infinity,
+          child: ElevatedButton(onPressed: () {}, child: const Text('Masuk'))),
     );
   }
 }
